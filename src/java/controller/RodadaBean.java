c8ed2cf0768076ffa0cb6cd3e136e70d1b7677cd
Drazzull <arthur.bilibio92@gmail.com>
2014-11-11 20:53:13 -0200

    Rodada

--- src/java/controller/RodadaBean.java
+++ src/java/controller/RodadaBean.java
@@ -6,11 +6,63 @@
 
 package controller;
 
+import dao.RodadaDao;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import model.Rodada;
+
 /**
  *
  * @author Drazzull
  */
+@ManagedBean
+@SessionScoped
 public class RodadaBean
 {
     
+    RodadaDao rodadaDao = new RodadaDao();
+    private Rodada rodada = new Rodada();
+
+    public Rodada getRodada()
+    {
+        return this.rodada;
+    }
+
+    public List<Rodada> getRodadas()
+    {
+        return this.rodadaDao.listar();
+    }
+
+    /*public List<Aposta> getAuditoria() throws Exception
+     {
+     return this.apostaDao.listarAuditoria();
+     }*/
+    public String salvar()
+    {
+        if (this.rodada.isEditando())
+        {
+            this.rodadaDao.atualizar(this.rodada);
+            this.rodada = new Rodada();
+            return "listarRodadas";
+        }
+
+        this.rodadaDao.salvar(this.rodada);
+        this.rodada = new Rodada();
+        return "listarRodadas";
+    }
+
+    public String editarItem(Rodada rodada)
+    {
+        this.rodada = rodada;
+        this.rodada.setEditando(true);
+        return "cadastroRodada";
+    }
+
+    public String excluirItem(Rodada rodada)
+    {
+        this.rodadaDao.excluir(rodada);
+        this.rodada = new Rodada();
+        return "listarRodadas";
+    }
 }
